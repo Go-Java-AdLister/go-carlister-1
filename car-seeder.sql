b7c088a6ff13935882f054f71c798912713c364b
Michael Dickens <dickens.michael64@gmail.com>
2022-07-20 16:48:07 -0500

    added seeder file to branch

---
+++ car-seeder.sql
@@ -0,0 +1,23 @@
+USE adlister_db;
+
+
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Michael Jackson','Thriller',1982,'Pop/Rock/R&B',47.3),
+       ('AC/DC','Back in Black',1980,'Hard rock',26.1),
+
+
+
+    SHOW DATABASES;
+
+SHOW TABLES;
+
+
+DESCRIBE albums;
+git status
+
+
+
+
+
+
+
