19bc9ce70ef7b876f8a9d8491928090e331b0239
Michael Dickens <dickens.michael64@gmail.com>
2022-07-21 09:39:46 -0500

    added seeder file for ads

--- car-seeder.sql
+++ car-seeder.sql
@@ -1,19 +1,26 @@
 USE adlister_db;
 
 
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Michael Jackson','Thriller',1982,'Pop/Rock/R&B',47.3),
-       ('AC/DC','Back in Black',1980,'Hard rock',26.1),
+INSERT INTO users (username, email, password)
+VALUES ('miked', 'miked@email.com', 'password1'),
+       ('chris', 'chris@email.com', 'password2'),
+        ('justin', 'justin@email.com', 'password3');
 
 
 
-    SHOW DATABASES;
-
-SHOW TABLES;
 
 
-DESCRIBE albums;
-git status
+INSERT INTO ads (user_id, title, description, year, make, color, model)
+VALUES (1, '1969 Camaro SS','Black 1969 Camaro SS with the Bully drag packace, blow 427, Devil rising intake stack, etc ','1969','Chevrolet', 'black', 'Camaro SS'),
+       (2, '1965 Mustang','Red 1965 Ford Mustang with a 302 Boss, Holly shifters, after market AC, etc ','1963','Ford', 'Red', 'Mustang'),
+       (3, '1969 Dodge Charger','1969 Hemi Orange Dodge Charger with a 396 blown Hemi, etc ','1969','Dodge', 'Hemi Orange','Charger');
+
+
+
+
+
+DESCRIBE ads;
+
 
 
 
